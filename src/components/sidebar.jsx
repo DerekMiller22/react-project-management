5aaf19ecfad06f1ba290736294715b7e0991cf66
dmillions <derek.m360@gmail.com>
2024-08-01 14:46:57 -0400

    ran project and added side bar component

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2>your projects</h2>
+      <div>
+        <button>Add project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
