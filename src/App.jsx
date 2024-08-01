f5bd98e2d8d3b753fbfac342e4fd381ff9399961
dmillions <derek.m360@gmail.com>
2024-08-01 15:14:03 -0400

    made btn its own component made no project selectd component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/sidebar";
 import NewProject from "./components/newProject";
+import NoProjectSelected from "./components/noProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
       <NewProject />
+      {/* <NoProjectSelected /> */}
     </main>
   );
 }
