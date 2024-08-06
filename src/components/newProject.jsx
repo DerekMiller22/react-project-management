140365b3c9b855d44ea84c45f8f097e57e43cb71
dmillions <derek.m360@gmail.com>
2024-08-06 14:58:13 -0400

    error modal for inputs created

--- src/components/newProject.jsx
+++ src/components/newProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Input from "./input";
+import Modal from "./modal";
 
 export default function NewProject({ onAddProject }) {
+  const modal = useRef();
   const titleRef = useRef();
   const descriptionRef = useRef();
   const dueDateRef = useRef();
@@ -11,35 +13,50 @@ export default function NewProject({ onAddProject }) {
     const enteredDescription = descriptionRef.current.value;
     const enteredDueDate = dueDateRef.current.value;
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAddProject({
       title: enteredTitle,
-      description: descriptionRef,
+      description: enteredDescription,
       dueDate: enteredDueDate,
     });
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input ref={titleRef} label="Title" />
-        <Input ref={descriptionRef} label="Description" textArea={true} />
-        <Input ref={dueDateRef} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="close Dialog">
+        <h2>Invalid Input</h2>
+        <p>input fields left blank</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={titleRef} label="Title" />
+          <Input ref={descriptionRef} label="Description" textArea={true} />
+          <Input ref={dueDateRef} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
